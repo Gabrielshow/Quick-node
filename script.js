18a5e5d3919015c2bb14dacc0d9c25722dc810f8
Gabrielshow <adefelashogbanmu@gmail.com>
2024-11-23 20:26:31 +0100

    git - auto push

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const node = "https://yolo-powerful-scion.quiknode.pro/4202501c4b6c2599eb96b6047
 const provider = new ethers.JsonRpcProvider(node);
 
 async function main() {
+    const wallet = ethers.Wallet.createRandom();
+    console.log('Mnemonic:', wallet.mnemonic.phrase);
+    console.log('Address:', wallet.address);
+    console.log('Private Key:', wallet.privateKey);
     const blockNumber = await provider.getBlockNumber();
     console.log("Block Number:", blockNumber);
 }
